1566018e472ed28c54bbf7ffa15352787503b14b
Nemanja Mosorinski <moshorinski@msn.com>
2015-01-09 12:51:05 +0100

    First Commit

---
+++ GenericLinkedList.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections;
+
+namespace LinkedListProject
+{
+//testing git ammened changes
+//branch testing
+    class GenericLinkedList<T> : CollectionBase
+    {
+        private class Element<T>
+        {
+            public T elementValue;
+            public Element<T> next;
+
+            public Element(T data, Element<T> next)
+            {
+                this.elementValue = data;
+                this.next = next;
+            }
+        }
+
+        private Element<T> head { get; set; }
+        private int size;
+        public int Count { get { return size; } }
+        public bool Empty { get { return this.size == 0; } }
+
+        /// <summary>
+        /// Method Add inserts new node into linked list on a specific position
+        /// </summary>
+        public T Add(int index, T o)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("Index: " + index);
+            }
+
+            if (index > size)
+            {
+                index = size;
+            }
+
+            Element<T> current = this.head;
+
+            if (this.Empty || index == 0)
+            {
+                this.head = new Element<T>(o, this.head);
+            }
+            else
+            {
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.next;
+                }
+                current.next = new Element<T>(o, current.next);
+            }
+            size++;
+            return o;
+        }
+
+        /// <summary>
+        /// Method AddAtTail adds new node at the tail of the linked list
+        /// </summary>
+        public T AddAtTail(T o)
+        {
+            return this.Add(size, o);
+        }
+
+        /// <summary>
+        /// Method PrintLinkedList prints all the elements of the defined linked list
+        /// </summary>
+        public void PrintLinkedList()
+        {
+            Console.WriteLine("LINKED LIST CONTENT IS:");
+            Console.WriteLine();
+
+            Element<T> temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.elementValue);
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Method Remove removes element with specified index in linked list
+        /// </summary>
+        public object Remove(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("Index: " + index);
+            }
+            if (index > size)
+            {
+                index = size - 1;
+            }
+            if (this.Empty)
+            {
+                return null;
+            }
+
+            Element<T> current = head;
+            object result = null;
+
+            if (index == 0)
+            {
+                result = current.elementValue;
+                head = current.next;
+            }
+            else
+            {
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.next;
+                }
+
+                result = current.next.elementValue;
+
+                current.next = current.next.next;
+            }
+            size--;
+            return result;
+        }
+
+        /// <summary>
+        /// Method GetElementByValue finds element in linked list with specified value
+        /// </summary>
+        public bool Contains(T value)
+        {
+            Element<T> temp = head;
+            bool elementFound = false;
+            while (!(elementFound = temp.elementValue.Equals(value)) && temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return elementFound;
+        }
+
+        /// <summary>
+        /// Method Get finds element in linked list on specified position and returns it's value if element exists
+        /// </summary>
+        public object Get(int index)
+        {
+            //if (index < 0)
+            //{
+            //    throw new ArgumentOutOfRangeException("Index: " + index);
+            //}
+            if (this.Empty)
+            {
+                return null;
+            }
+                
+            if (index >= this.size)
+                index = this.size - 1;
+
+            Element<T> current = head;
+
+            for (int i = 0; i < index; i++)
+            {
+                current = current.next;
+            }
+            return current.elementValue;
+        }
+
+        /// <summary>
+        /// Method Clear removes all elements from linked list
+        /// </summary>
+        public void Clear()
+        {
+            this.head = null;
+        }
+
+        /// <summary>
+        /// Method IndexOf finds element with specified value and return it's index
+        /// </summary>
+        public int IndexOf(T o)
+        {
+            Element<T> current = this.head;
+
+            for (int i = 0; i < this.size; i++)
+            {
+                if (current.elementValue.Equals(o))
+                {
+                    return i;
+                }
+                current = current.next;
+            }
+            return -1;
+        }
+
+        //Researching possibilites
+        public void SpecialAdd(T o)
+        {
+            InnerList.Add(o);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace LinkedListProject
+{
+//branch testing 2
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericLinkedList<int> numberList = new GenericLinkedList<int>();
+
+
+            numberList.AddAtTail(1);
+            numberList.AddAtTail(2);
+            numberList.AddAtTail(3);
+            numberList.Add(2, 4);
+            numberList.Add(0, 6);
+
+            Console.WriteLine("BEFORE REMOVING:");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+            numberList.PrintLinkedList();
+
+            Console.WriteLine("AFTER REMOVING:");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+            numberList.Remove(3);
+            numberList.PrintLinkedList();
+
+            GenericLinkedList<string> stringList = new GenericLinkedList<string>();
+
+            Console.WriteLine("BEFORE REMOVING:");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+            stringList.Add(0, "First");
+            stringList.Add(1, "Second");
+            stringList.Add(2, "Third");
+            stringList.AddAtTail("Fourth");
+            stringList.Add(2, "Fifth");
+            stringList.PrintLinkedList();
+
+            Console.WriteLine("AFTER REMOVING:");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+            stringList.Remove(1);
+            stringList.PrintLinkedList();
+            
+            Console.WriteLine();
+            Console.WriteLine("PLEASE ENTER THE VALUE OF THE ELEMENT YOU WANT TO FIND IN LINKED LIST:");
+            
+            var item = Console.ReadLine();
+            
+            if (stringList.Contains(item) == true)
+            {
+                Console.WriteLine("ELEMENT WITH VALUE: {0} IS FOUND IN THE LIST!", item);
+            }
+            else
+            {
+                Console.WriteLine("ELEMENT WITH VALUE: {0} IS NOT FOUND IN THE LIST!", item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PLEASE ENTER POSITION OF ELEMENT IN LINKED LIST:");
+            string position = Console.ReadLine();
+            int pos;
+            
+            if (int.TryParse(position, out pos))
+            {
+                if(pos > numberList.Count)
+                {
+                    Console.WriteLine("INDEX IS TOO LARGE. INDEX OF LAST ELEMENT IN THE LIST IS: {0}", numberList.Count.ToString());
+                }
+                else if (numberList.Count < pos)
+                {
+                    Console.WriteLine("POSITION OF ELEMENT IS OUT OF RANGE. ELEMENT ON POSITION: {0} IS NOT EXISTING. \nELEMENT ON THE LAST POSITION HAS INDEX: {1}", pos, numberList.Count.ToString());
+                }
+                else if (pos < 0)
+                {
+                    Console.WriteLine("POSITION OF ELEMENT IS OUT OF RANGE. ELEMENT POSITION CAN NOT BE NEGATIVE NUMBER!");
+                }
+                else
+                {
+                    Console.WriteLine("ELEMENT AT POSITION {0} HAS VALUE: {1}", position, numberList.Get(pos));
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("WRONG FORMAT! PLEASE ENTER A NUMBER VALUE.");
+            }
+            Console.WriteLine();
+            Console.WriteLine("ENTER THE VALUE OF ELEMENT TO GET IT'S INDEX:");
+            var elementIndex = Console.ReadLine();
+            if (stringList.IndexOf(elementIndex) != -1)
+            {
+                Console.WriteLine("ELEMENT: {0} HAS INDEX: {1}", elementIndex, stringList.IndexOf(elementIndex));
+            }
+            else
+            {
+                Console.WriteLine("ELEMENT IS NOT EXISTING!");
+            }
+
+            GenericLinkedList<int> specialList = new GenericLinkedList<int>();
+
+            specialList.SpecialAdd(1);
+            specialList.SpecialAdd(5);
+
+            Console.ReadLine();
+        }
+    }
+}
